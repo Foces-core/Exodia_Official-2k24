2684d3d54f91116841b954698feb4784b3545d91
nandkishorr <rachulr7@gmail.com>
2024-01-27 00:31:47 +0530

    responsiveness

--- src/pages/Mentors/Mentors.jsx
+++ src/pages/Mentors/Mentors.jsx
@@ -19,7 +19,7 @@ function Mentors() {
             </div>
             {/* Heading */}
             <div className="relative w-14 max-[768px]:w-fit max-[768px]:h-fit max-[768px]:py-10">    
-                <p className="absolute text-white rotate-90 text-8xl theme h-fit w-fit top-[50%] max-[550px]:text-[15vw] -right-[30vh] max-[768px]:rotate-0 max-[768px]:relative max-[768px]:right-0 max-[768px]:top-0">MENTORS</p>
+                <p className="absolute text-white rotate-90 text-8xl theme h-fit w-fit top-[50%] max-[550px]:text-5xl -right-[30vh] max-[768px]:rotate-0  max-[768px]:relative max-[768px]:right-0 max-[768px]:top-0 ">MENTORS</p>
             </div>
             {/* Mentors Background */}
             <img src={MentorImg} alt="" className="absolute bottom-0 w-full"/>
--- src/pages/Stacks/Stacks.jsx
+++ src/pages/Stacks/Stacks.jsx
@@ -20,8 +20,8 @@ function Stacks() {
     <div className="relative z-0 flex items-center justify-center w-full h-screen overflow-hidden" id="Stacks">
       {/* ///////////////MID CONTENT////////////// */}
       <div className="flex flex-col h-[600px] w-[900px] items-center justify-center ">
-        <p className="theme text-8xl text-[#1D9D51] justify-center">STACKS</p>
-        <p className="mt-5 bg-[#1D9D51] rounded-lg w-96 h-8 text-white font-bold text-center text-lg">Choose your adventure !</p>
+        <p className="theme text-8xl text-[#1D9D51] justify-center max-md:text-5xl">STACKS</p>
+        <p className="mt-5 bg-[#1D9D51] rounded-lg text-white font-bold text-center text-lg max-sm:text-sm h-5 w-64 rounded-md font-medium sm:h-8 w-96">Choose your adventure !</p>
         <div className="flex m-5 mt-28 gap-10">
           {stackData && stackData.Stack_name ? (
             stackData.Stack_name.map((item) => (
