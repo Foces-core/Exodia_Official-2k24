ec545828888f4934a3db3c5392bf139e963968bb
Harikrishna A <Harikichus2018@gmail.com>
2024-01-28 18:01:18 +0530

    added aos on folders

--- src/Componets/Folder.jsx
+++ src/Componets/Folder.jsx
@@ -13,10 +13,10 @@ function Folder(props) {
 
   return (
 
-<div onClick={Setvalues}>
+<div onClick={Setvalues }data-aos='fade-up'>
     <div className=" relative flex justify-end items-end scale-75 cursor-pointer group max-md:scale-[0.66] max-sm:scale-x-75">
-    <img className="absolute -top-9 z-10 transition-transform transform group-hover:scale-110 max-md:-top-7" src={coverback} alt="file" />
-    <img className="absolute z-20  scale-90 origin-bottom-right transition-transform transform group-hover:rotate-12  max-md:scale-90" src={file} alt="file" />
+    <img className="absolute z-10 transition-transform transform -top-9 group-hover:scale-110 max-md:-top-7" src={coverback} alt="file" />
+    <img className="absolute z-20 transition-transform origin-bottom-right transform scale-90 group-hover:rotate-12 max-md:scale-90" src={file} alt="file" />
     <img className="z-30 transition-transform transform group-hover:scale-110 max-md:scale-10" src={coverfront} alt="file" />
       </div>
   <p className="text-white text-center font-semibold max-sm:text-[10px]">{props.name}</p>
