4f42d688e6d9d2dc2b7bd5f5f24d737369d2d9e5
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2024-01-28 16:35:43 +0530

    Added Animations

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -18,6 +18,7 @@ function About() {
                           <ReactTyped strings={["Welcome to EXODIA- where Innovation meets Inspiration! Exodia is an all-Kerala event that brings together tech enthusiasts and creative minds to explore Artificial Intelligence (AI), Augmented Reality (AR), and Game Development. This immersive experience empowers attendees with innovative knowledge, hands-on workshops led by our esteemed mentors, and unparalleled networking opportunities. EXODIA is a joint effort by IEDC, FOCES, and MULEARN -the 3 leading forums of the College Of Engineering Chengannur. Taking place on February 17th and 18th at the prestigious CEC Campus, this extravaganza is a fusion of intellect, talent, and excitement. Join us at EXODIA, where you can fuel your passion for technology, connect with like-minded individuals, and stay at the forefront of the latest AI, AR, and Game Development advancements."]}
                               typeSpeed={20}
                               loop={false}
+                              showCursor={false}
                           />
               
             </p>
