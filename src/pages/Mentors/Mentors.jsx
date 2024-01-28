8d1186de2b63f4a1a8d29144ba9ea4325e8f8937
SHIJIN ABRAHAM <shijinabraham2003@gmail.com>
2024-01-28 21:20:06 +0530

    Updated Mentors

--- src/pages/Mentors/Mentors.jsx
+++ src/pages/Mentors/Mentors.jsx
@@ -1,6 +1,8 @@
 import MentorCard from "../../Componets/MentorCard";
 import AshwinImg from "../../assets/Mentors/Ashwin Dev MP.png";
 import MentorImg from "../../assets/Mentors/Mentors.svg";
+import AromalImg from "../../assets/Mentors/Aromal.png";
+import JerilImg from "../../assets/Mentors/Jeril.png";
 
 function Mentors() {
     return(
@@ -11,11 +13,11 @@ function Mentors() {
             <div className="flex flex-col items-center gap-20 px-[15vh] max-[428px]:px-6 max-[375px]:px-2 max-[768px]:w-full max-[768px]:px-12 max-[768px]:gap-32 my-10">
                 <MentorCard Name="ASWIN DEV MP" Desc="Spark Playtime AR Competition Winner" Topic="Augmented Reality" Image={AshwinImg} Index={0} />
                 {/* Container to align odd card */}
-                <div className="mr-[15vh] max-[768px]:mr-0 max-[768px]:w-full max-[768px]:flex max-[768px]:justify-center">
-                <MentorCard Name="ASWIN DEV MP" Desc="Spark Playtime AR Competition Winner" Topic="Generative AI" Image={AshwinImg} Index={1}/>
-                </div>
                 
-                <MentorCard Name="ASWIN DEV MP" Desc="Spark Playtime AR Competition Winner" Topic="Game Development" Image={AshwinImg} Index={2}/>
+                <MentorCard Name="AROMAL S SANTHOSH" Desc="SDE @IBM ISL [Data & AI ~ IBM watsonx] " Topic="Generative AI" Image={AromalImg} Index={1}/>
+                
+                
+                <MentorCard Name="JERIL MONZI JACOB" Desc="Software Engineer @ Founding Minds Software Pvt Ltd" Topic="Game Development" Image={JerilImg} Index={2}/>
             </div>
             {/* Heading */}
             <div className="relative w-14 max-[768px]:w-fit max-[768px]:h-fit max-[768px]:py-10">    
