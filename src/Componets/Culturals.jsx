d625b04f3e3719bdb8d529471843ee090ee38365
akashmn <akash003nandan@gmail.com>
2024-01-30 11:52:36 +0530

    updated the links

--- src/Componets/Culturals.jsx
+++ src/Componets/Culturals.jsx
@@ -8,11 +8,11 @@ function Culturals(){
         <div className="flex  justify-center max-[768px]:translate-x-[10%] max-[768px]:justify-start max-[768px]:w-[80%] w-full h-[60vw] max-[768px]:h-[90vw] ">
             <div className="relative border w-[30vw] h-[20vw] border-whtie max-[768px]:w-[45vw] max-[768px]:h-[30vw] " data-aos='fade-right'>
                 <img src={_01} alt=""  className='w-full h-full'/>
-                <p className="w-full text-white bg-gradient-to-r from-[#9329FE] to-transparent px-5 absolute bottom-0 right-0"> CEC Band</p>
+                <p className="w-full text-white bg-gradient-to-r from-[#9329FE] to-transparent px-5 absolute bottom-0 right-0"> Music Band</p>
                 <div className="absolute border w-[30vw] h-[20vw] border-white top-[80%] -right-[80%] max-[768px]:w-[45vw] max-[768px]:h-[30vw]" data-aos='fade-left'>
                     <div className="relative w-full h-full">
                         <img src={_02} alt="" className='w-full h-full'/>
-                        <p className="w-full text-white bg-gradient-to-l text-right from-[#9329FE] px-5 to-transparent absolute bottom-0 right-0"> Campfire</p>
+                        <p className="w-full text-white bg-gradient-to-l text-right from-[#9329FE] px-5 to-transparent absolute bottom-0 right-0"></p>
                         <div className="absolute border w-[30vw] h-[20vw] border-white top-[80%] -left-[80%] max-[768px]:w-[45vw] max-[768px]:h-[30vw]" data-aos='fade-right'>
                             <div className="relative w-full h-full">
                                 <img src={_03} alt="" className='w-full h-full'/>
--- src/pages/Mentors/Mentors.jsx
+++ src/pages/Mentors/Mentors.jsx
@@ -14,7 +14,7 @@ function Mentors() {
                 <MentorCard Name="ASWIN DEV MP" Desc="Spark Playtime AR Competition Winner" Topic="Augmented Reality" Image={AshwinImg} Index={0} link="https://www.linkedin.com/in/aswin-dev-m-p-4206791b3?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"/>
                 {/* Container to align odd card */}
                 
-                <MentorCard Name="AROMAL S SANTHOSH" Desc="SDE @IBM ISL [Data & AI ~ IBM watsonx] " Topic="Generative AI" Image={AromalImg} Index={1} link="https://www.linkedin.com/in/aromalsanthosh?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"/>
+                <MentorCard Name="AROMAL S" Desc="SDE @IBM ISL [Data & AI ~ IBM watsonx] " Topic="Generative AI" Image={AromalImg} Index={1} link="https://www.linkedin.com/in/aromalsanthosh?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"/>
                 
                 
                 <MentorCard Name="JERIL MONZI JACOB" Desc="Software Engineer @ Founding Minds Software Pvt Ltd" Topic="Game Development" Image={JerilImg} Index={2} link="https://www.linkedin.com/in/jerilmj?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app"/>
@@ -24,7 +24,7 @@ function Mentors() {
                 <p className="absolute text-white rotate-90 text-8xl theme h-fit w-fit top-[50%] max-md:text-5xl max-md:mb-20 -right-[30vh] max-[768px]:rotate-0  max-[768px]:relative max-[768px]:right-0 max-[768px]:top-0 ">MENTORS</p>
             </div>
             {/* Mentors Background */}
-            <img src={MentorImg} alt="" className="absolute bottom-1 z-0 w-full max-md:bottom-96"/>
+            <img src={MentorImg} alt="background-image" className="absolute bottom-1 z-0 w-full max-md:bottom-96"/>
         </div>
         
         </>
